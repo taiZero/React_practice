6f6129557e8e947f67d537dc78d0f7c54b971ba8
taiZero <tairiruzero3@gmail.com>
2021-07-27 18:35:55 +0900

    reactStop

--- src/index.js
+++ src/index.js
@@ -3,24 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CounterApp from './CounterApp';
-import reportWebVitals from './reportWebVitals';
-import { Switch,Route,Router,Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from "history";
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history ={hist}>
-      <Switch>
-        <Route path="/app" compornent={App} />
-        <Route path="/counterapp" compornent={CounterApp} />
-        <Redirect from="/" to="/app" />
-      </Switch>
+  <Router history={hist}>
+    <Switch>
+      <Route path="/app" compornent={App} />
+      <Route path="/counterapp" compornent={CounterApp} />
+      <Redirect from="/" to="/app" />
+    </Switch>
   </Router>,
+ /*
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  */
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
